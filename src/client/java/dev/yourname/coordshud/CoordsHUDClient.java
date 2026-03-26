49f1372d2ca06ab25faf0aca61063c8c88615fdc
Muhammed İkbal <m.ikbal0302l@gmail.com>
2026-03-26 15:31:57 +0300

    feat: add Fabric mod scaffold and HUD project layout

---
+++ src/client/java/dev/yourname/coordshud/CoordsHUDClient.java
@@ -0,0 +1,10 @@
+package dev.yourname.coordshud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoordsHUDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
